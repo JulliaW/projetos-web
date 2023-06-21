9d354f82c34c53d3f15111f3c34186a8a5ec34aa
JulliaW <weberjullia44@gmail.com>
2023-06-21 17:34:00 -0300

    Adicionando + projetos

---
+++ BuscaCEP/BuscaCEP/Controllers/CepController.cs
@@ -0,0 +1,32 @@
+﻿using BuscaCEP.Models;
+using BuscaCEP.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuscaCEP.Controllers
+{
+    public class CepController : Controller
+    {
+        private readonly CorreiosService _correiosService;
+
+        public CepController(CorreiosService correiosService)
+        {
+            _correiosService = correiosService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> BuscarEnderecoPorCep(string cep)
+        {
+            Endereco? endereco = await _correiosService.BuscarEnderecoPorCep(cep);
+            if (endereco == null)
+            {
+                TempData["Mensagem"] = "CEP <strong> não </strong> encontrado";
+            }
+            return View("Index", endereco);
+        }
+    }
+}
